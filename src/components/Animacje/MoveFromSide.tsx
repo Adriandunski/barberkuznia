8b099100bf6565dc3bcc0a1073a1d54d94170893
adriandunski <mrduniak@icloud.com>
2024-02-02 13:34:19 +0100

    end

---
+++ src/components/Animacje/MoveFromSide.tsx
@@ -0,0 +1,29 @@
+import {AnimationControls, motion, useAnimation} from "framer-motion";
+import {useInView} from "react-intersection-observer";
+import {useEffect} from "react";
+
+export default function MoveFromSide({hiddenX, children} : {hiddenX: number, children: any}) {
+    const variants = {visible: {x: 0, opacity: 1}, hidden: {x: hiddenX, opacity: 0}};
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible")
+        }
+    }, [controls, inView]);
+
+    return(
+        <div ref={ref} className={'h-full w-full'}>
+            <motion.div
+                variants={variants}
+                initial={"hidden"}
+                transition={{type: "spring", bounce: 0.5, delay: 0.5, duration: 2}}
+                animate={controls}
+                className={"w-full h-full"}
+            >
+                {children}
+            </motion.div>
+        </div>
+    );
+}
